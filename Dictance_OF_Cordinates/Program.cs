0439a85d58fe24cd367c784e2fea282b5fe671f7
Nurik <you@example.com>
2022-10-25 18:37:39 +0300

    commiting changes

--- Dictance_OF_Cordinates/Program.cs
+++ Dictance_OF_Cordinates/Program.cs
@@ -19,12 +19,25 @@ void DistanceOfPoints( int x1, int y1, int x2, int y2)
     {
        double distance = Math.Sqrt((x1 - x2)* (x1 -x2) + (y1 - y2)*(y1 - y2));
       Console.WriteLine(distance);
-    }   
+    } 
+      
     else if ( x1 < x2 && y1 < y2 )
     {
         
         double distance = Math.Sqrt((x2 - x1)* (x2 -x1) + (y2 - y1)*(y2 - y1));
        Console.WriteLine(distance);
+    }
+
+     else if ( x1 < x2 && y1 > y2)
+    {
+        double distance = Math.Sqrt((x2 - x1)* (x2 -x1) + (y1 - y2)*(y1 - y2));
+        Console.WriteLine(distance);
+    }
+
+    else if ( x1 > x2 && y1 < y2)
+    {
+        double distance = Math.Sqrt((x1 - x2)* (x1 -x2) + (y1 - y2)*(y1 - y2));
+        Console.WriteLine(distance);
     }
   
 }
