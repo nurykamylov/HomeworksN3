f292603461acb01b1344e5038e5a1f9b3b80d43c
Nurik <you@example.com>
2022-10-26 03:23:26 +0300

    cube of numbers

---
+++ Cube_Natural_Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 23
+Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.Write("Enter the natural number:");
+int a = int.Parse(Console.ReadLine()!);
+double i = 1;
+while( i <= a)
+{
+    Console.Write(Math.Pow(i, 3));
+    Console.Write(",");
+    i++;
+}
+
