1e30969e32f4d795ab4632e00cb985e513ad47f7
Nurik <you@example.com>
2022-10-28 21:41:56 +0300

    simple integers done

---
+++ Simple_Integers/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Дополнительно.
+Напишите программу, которая принимает на вход число (N) 
+и выдает таблицу простых чисел от 1 до N.
+11 -> 2, 3, 5 , 7, 11*/
+
+Console.WriteLine("Enter natural number: ");
+int a = int.Parse(Console.ReadLine()!);
+  bool isSimple(int N)
+   {
+            
+      for (int i = 2; i <= (int)(N / 2); i++)
+       {
+         if (N % i == 0)
+         return false;
+       }
+      return true;
+   }
+void Main(int a)
+{
+
+      
+for (int i = 1; i <= a; i++)
+{
+   if (isSimple(i))
+   {
+   Console.Write($"{i},");
+   }
+}
+}
+  
+    
+Main(a);
